41b88143029d7549ae3de861ba97d1887629935f
dipo93 <andrea.difonzo93@gmail.com>
2016-01-07 18:15:18 +0100

    updated feature storage

--- project/src/main_package/CommentHandling.java
+++ project/src/main_package/CommentHandling.java
@@ -5,13 +5,19 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+//import java.math.BigInteger;
 import java.util.List;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 import main_package.Auth;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+//import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.YouTube;
+//import com.google.api.services.youtube.model.Channel;
+//import com.google.api.services.youtube.model.ChannelListResponse;
 import com.google.api.services.youtube.model.Comment;
 import com.google.api.services.youtube.model.CommentThread;
 import com.google.api.services.youtube.model.CommentThreadListResponse;
@@ -29,7 +35,7 @@ public class CommentHandling {
      * YouTube Data API requests.
      */
     private static YouTube youtube;
-
+    
     /**
      * List, reply to comment threads; list, update, moderate, mark and delete
      * replies.
@@ -137,7 +143,6 @@ public class CommentHandling {
             System.err.println("GoogleJsonResponseException code: " + e.getDetails().getCode()
                     + " : " + e.getDetails().getMessage());
             e.printStackTrace();
-
         } catch (IOException e) {
             System.err.println("IOException: " + e.getMessage());
             e.printStackTrace();
@@ -160,7 +165,7 @@ public class CommentHandling {
 
         return videoId;
     }
-
+    
     /*
      * Prints on the console all the informations about the comment
      * */
@@ -170,14 +175,12 @@ public class CommentHandling {
     	System.out.println("Author Channel URL: "+videoComment.getSnippet().getAuthorChannelUrl());
     	System.out.println("Channel Subscribers Count: "+ FeatureProcessing.getChannelSubscribers(youtube, videoComment.getSnippet().getAuthorChannelId().getValue()));
     	System.out.println("Video ID: "+videoComment.getSnippet().getVideoId());
-    	//if the comment is not a reply, the following entry will be "null"
     	System.out.println("Parent ID: "+videoComment.getSnippet().getParentId());
     	System.out.println("Text Display: "+videoComment.getSnippet().getTextDisplay());
     	System.out.println("Moderation Status: "+videoComment.getSnippet().getModerationStatus());
     	System.out.println("Like Count: "+videoComment.getSnippet().getLikeCount());
-    	if (cThread != null) {
+    	if(cThread != null)
     		System.out.println("Reply Count: "+cThread.getSnippet().getTotalReplyCount());
-    	}
     	System.out.println("Published At: "+videoComment.getSnippet().getPublishedAt());
     	System.out.println("Updated At: "+videoComment.getSnippet().getUpdatedAt());
         System.out.println("Video Title: " + video.getSnippet().getTitle());
@@ -197,25 +200,21 @@ public class CommentHandling {
      * */
     private static void storeVideoComment(Comment videoComment, Video video, CommentThread cThread, BufferedWriter b) throws IOException {
     	b.write("{\n");
-    	if (cThread != null) {
+    	if(cThread != null)
     		b.write("\"isReply\" : \"false\",\n");
-    	}
-    	else {
+    	else
     		b.write("\"isReply\" : \"true\",\n");
-    	}
     	b.write("\"moderationStatus\" : \""+videoComment.getSnippet().getModerationStatus()+"\",\n");
     	b.write("\"authorChannelId\" : \""+videoComment.getSnippet().getAuthorChannelId().getValue()+"\",\n");
     	b.write("\"authorDisplayName\" : \""+videoComment.getSnippet().getAuthorDisplayName()+"\",\n");
     	b.write("\"authorChannelUrl\" : \""+videoComment.getSnippet().getAuthorChannelUrl()+"\",\n");
     	b.write("\"channelSubscribersCount\" : \""+ FeatureProcessing.getChannelSubscribers(youtube, videoComment.getSnippet().getAuthorChannelId().getValue())+"\",\n");
     	b.write("\"videoId\" : \""+videoComment.getSnippet().getVideoId()+"\",\n");
-    	//parentId is null if the videoComment isn't a reply
     	b.write("\"parentId\" : \""+videoComment.getSnippet().getParentId()+"\",\n");
     	b.write("\"textDisplay\" : \""+videoComment.getSnippet().getTextDisplay()+"\",\n");
     	b.write("\"likeCount\" : \""+videoComment.getSnippet().getLikeCount()+"\",\n");
-    	if (cThread != null) {
+    	if(cThread != null)
     		b.write("\"replyCount\" : \""+cThread.getSnippet().getTotalReplyCount()+"\",\n");
-    	}
     	b.write("\"publishedAt\" : \""+videoComment.getSnippet().getPublishedAt()+"\",\n");
     	b.write("\"updatedAt\" : \""+videoComment.getSnippet().getUpdatedAt()+"\",\n");
     	b.write("\"videoTitle\" : \"" + video.getSnippet().getTitle()+"\",\n");
@@ -230,4 +229,6 @@ public class CommentHandling {
         b.write("\",\n");
     	b.write("},\n\n");
     }
+    
+
 }
--- project/src/main_package/FeatureProcessing.java
+++ project/src/main_package/FeatureProcessing.java
@@ -4,13 +4,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Channel;
 import com.google.api.services.youtube.model.ChannelListResponse;
 import com.google.api.services.youtube.model.Comment;
 import com.google.api.services.youtube.model.CommentThread;
+//import com.google.api.services.youtube.model.CommentThread;
 import com.google.api.services.youtube.model.Video;
 
 
@@ -31,9 +35,26 @@ public class FeatureProcessing {
 			cp.write("replies: " + getNumberOfReplies(cThread) +"\n");
 		}
 		cp.write("isCommentFromTheVideoAuthor: "+ isCommentFromTheVideoAuthor(comment, video) +"\n");
+
+		cp.write("authorNameInComment: " + searchAuthorNameInComment(comment) + "\n");
+
+		cp.write("intervalBetweenVideoAndComment: " + intervalBetweenVideoAndCommentPublish(comment, video) + "\n");
+
+		int[] ss = new int[2];
+		ss = numberMentionAndHashtags(comment);
+		cp.write("mentionTagsNumber: " + ss[0] + "\n");
+		cp.write("hashtagsNumber: " + ss[1] + "\n");
+
+		String authorId = comment.getSnippet().getAuthorChannelId().getValue();
+		cp.write("authorChannelSubscribersCount: " + getChannelSubscribers(youtube, authorId) + "\n");
+
+		cp.write("videoMomentReference: " + videoMomentReference(comment) + ",\n");
 		
 		cp.write("\n");
 		//a fine metodo vai a capo
+		
+	   
+
 	}
 	
 	
@@ -66,20 +87,68 @@ public class FeatureProcessing {
 	/* returns suspicious keywords
 	 * this method must be used on the comment text and also on the author name*/
 	public static String keywords(String s) {
+		return "";
 		
 	}
+	/* process comment data to create a json in the text with all the features needed
+     */
+    
 	
-	 /**
+	
+	public static String searchAuthorNameInComment(Comment videoComment) {
+    	String author = videoComment.getSnippet().getAuthorDisplayName();
+    	String text = videoComment.getSnippet().getTextDisplay();
+    	
+    	Pattern pattern = Pattern.compile(author);
+    	Matcher matcher = pattern.matcher(text);
+    	boolean res = matcher.find();
+		
+    	return res==true ? "true" : "false";
+	}
+	
+
+	public static String intervalBetweenVideoAndCommentPublish(Comment videoComment, Video video) {
+		DateTime videoDate = video.getSnippet().getPublishedAt();
+		DateTime commentDate = videoComment.getSnippet().getPublishedAt();
+		
+		long diff = commentDate.getValue() - videoDate.getValue();
+		long diffSeconds = diff / 1000 % 60;
+        long diffMinutes = diff / (60 * 1000) % 60;
+        long diffHours = diff / (60 * 60 * 1000);
+        int diffDays = (int) (diff) / (1000 * 60 * 60 * 24);
+        
+        String res = diffDays + " days, " + diffHours + " hours, " + diffMinutes + " minutes, " + diffSeconds + " seconds";
+		
+        return res;
+	}
+	
+
+	public static int[] numberMentionAndHashtags(Comment videoComment) {
+    	String text = videoComment.getSnippet().getTextDisplay();
+    	int tags[] = {0, 0};	//[0] is mention tags (+) and [1] is hashtags (#)
+    	
+    	Pattern mentions = Pattern.compile("+[a-zA-Z_0-9]*");
+    	Matcher matcherM = mentions.matcher(text);
+    	while(matcherM.find())
+    		tags[0]++;
+    	
+    	Pattern hashtags = Pattern.compile("#[a-zA-Z_-0-9]*");
+    	Matcher matcherH = hashtags.matcher(text);
+    	while(matcherH.find())
+    		tags[1]++;
+    	
+		return tags;
+	}
+    
+    /**
      * With the channelId, returns a String with the subscribers' number
      */
     public static String getChannelSubscribers(YouTube youtube, String channeId) {
-    
         try {
             // Call the YouTube Data API's channels.list method to
             // retrieve channels.
             ChannelListResponse channelListResponse = youtube.channels().list("statistics")
             		.setId(channeId).execute();
-            
             List<Channel> channelList = channelListResponse.getItems();
             
             if (channelList.isEmpty())
@@ -87,9 +156,7 @@ public class FeatureProcessing {
             
             Channel channel = channelList.get(0);
             BigInteger subscribers = channel.getStatistics().getSubscriberCount();
-            
             return subscribers.toString();
-        
         
         } catch (GoogleJsonResponseException e) {
            System.err.println("GoogleJsonResponseException code: " + e.getDetails().getCode()
@@ -107,5 +174,18 @@ public class FeatureProcessing {
             return ("Throwable: " + t.getMessage());
         }
     }
+    
+    
+	public static String videoMomentReference(Comment videoComment) {
+		String text = videoComment.getSnippet().getTextDisplay();
+		
+		Pattern pattern = Pattern.compile("([0-9]{1,2}:)?[0-9]{1,2}:[0-9]{2}");
+    	Matcher matcher = pattern.matcher(text);
+    	boolean res = matcher.find();
+		
+		return res==true ? "true" : "false";
+	}
+    
+	
 	
 }
